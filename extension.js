72ca819bd1ea3592943091a26c080af940f5b1ad
JohnBostater <jdbost8r@gmail.com>
2026-05-17 02:03:12 -0400

    Saving progress.
    Outlining functions, & program flow, as well as how I want to proceed with capturing default-theme data & using it.

--- extension.js
+++ extension.js
@@ -5,23 +5,66 @@
 
 [Description]:
     Randomize the theme to whatever
+
+
+[TO DO!!]
+
+  -  Have three arrays containing themes:  allThemes, lightThemes, darkThemes
+
+  -  Allow user's the option to set a "Default theme" that they can quickly apply with a button
+
+  -  Allow user's the option to randomly switch themes on a timer (a feat that similar extensions have)
+
 */
 
 
-//[Variables/Reqs]
-//---------------------------------
-  const vscode = require('vscode');
-//---------------------------------
+//[Global]
+//----------------------------------------
+
+  //Reqs
+    const vscode = require('vscode');
+    const os = require('os');
+    const path = require('path');
+
+  //Variables
+    var userName = os.userInfo().username;
+
+//----------------------------------------
 
 
-//[System Functions]
+//[Allocations before Activation]
+//---------------------------------------------------------------------------
+
+
+  //Find the .json file that contains all of the default themes 
+  //    Search the directories past:    "C:\Users\<NAME>\AppData\Local\Programs\Microsoft VS Code\<directory>"
+  //      We are doing this to see if they contain:   "<directory>\resources\app\extensions\theme-defaults\package.json" 
+  //
+  //  There could also NOT be a directory between the two, so account for that too!
+  //
+  //    a lot of   path.exist()   coming up im sure
+  //
+    //Code here..
+
+
+  //Load the .json data from the dump 
+  //  maybe just gather the array of theme name's directly from:   ["contributes"]["themes"][INDEX OF THEME]["label"]
+    //Code here..
+
+
+//---------------------------------------------------------------------------
+
+
+//[System Function]
 //--------------------------------------------------------------------------------------------
 
   //[Runs upon Activation of the Extension]
     function activate(context) {
 
+
       //Register the Extension to the tree
         vscode.window.registerTreeDataProvider( "changeOfTheme", new ThemeChanger() );
+
 
       //Push events to subscribers of action-event handlers
         context.subscriptions.push(
@@ -29,6 +72,12 @@
           //[Dark Mode Command Registry]
             vscode.commands.registerCommand("darkMode", (item) => {
 
+
+//[TO DO!!]
+//  Do this for the other branches below too
+
+              //Randomly Generate a number between:  0  and  Length(darkThemeArray)-1 
+                //Code here...
 
               //Inform the user of their choice
                 vscode.window.showInformationMessage("Random Dark Theme Applied!");
@@ -49,6 +98,8 @@
           //[Any Theme Mode Command Registry]
             vscode.commands.registerCommand("anyMode", (item) => {
 
+
+
               //Inform the user of their choice
                 vscode.window.showInformationMessage("Any Random Theme Applied!");
 
@@ -56,6 +107,28 @@
 
         );
     }
+
+//--------------------------------------------------------------------------------------------
+
+
+//[Other Functions]
+//--------------------------------------------------------------------------------------------
+
+
+//[TO DO!!]
+//  FINISH THIS FUNCTION
+
+  //Randomly generate a number between 0  & the number given
+  //  this will be the index of our theme
+    function GenerateRandomNumber(randNumRange){ return 0; }
+
+
+
+//[TO DO!!]
+//  FINISH THIS FUNCTION
+
+  //Collect all of the themes either:    [Dark] : True    or    [Light] : False
+    function GenerateRandomNumber(themeType){ return 0; }
 
 //--------------------------------------------------------------------------------------------
 
